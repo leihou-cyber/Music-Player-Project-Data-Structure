ccee559c90e7c06c70e69064d115fee813ff3960
taxuslor <liteor@outlook.com>
2024-03-06 11:44:21 +0800

    commit basic files

---
+++ deque/LinkedListDeque.java
@@ -0,0 +1,23 @@
+package deque;
+
+public class LinkedListDeque {
+    public LinkedListDeque(){
+
+    }
+
+    public T getRecursive(int index){
+
+    }
+
+    public T add(){
+
+    }
+
+    public T get(){
+
+    }
+
+    public T size(){
+
+    }
+}
